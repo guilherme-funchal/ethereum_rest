457d6398f4e8d308e8a64c1ed9859ad7c279d156
Guilherme Funchal da Silva <guilherme.funchal@gmail.com>
2022-09-23 07:44:13 -0300

    Atualização no serviços REST
    Atualização no serviços REST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var address = "http://127.0.0.1:8545"
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -13,11 +14,11 @@ require("dotenv").config();
 
 const CONTACT_ABI = require('./config');
 const CONTACT_ADDRESS = require('./config');
+const { application } = require('express');
 
 
 app.get('/dono', async function(req, res) {
-    
-    var web3 = new Web3('http://127.0.0.1:8545');
+    var web3 = new Web3(address);
     var contratoInteligente = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI, CONTACT_ADDRESS.CONTACT_ADDRESS);
     let owner = await contratoInteligente.methods.owner().call(function (err, res) {
       if (err) {
@@ -26,54 +27,51 @@ app.get('/dono', async function(req, res) {
       }
       console.log("O dono é: ", res)
     }); 
-    res.status(200).send(owner);
-
+    res.status(200).send(JSON.stringify(owner));
 });
 
-app.get('/transacao', async function(req, res) {
+app.get('/transacoes', async function(req, res) {
   try {
-    var web3 = new Web3('http://127.0.0.1:8545');  
-  //  let block = new web3.eth.getTransactionCount("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").then(console.log);
+    var web3 = new Web3(address);  
     var contratoInteligente = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI, CONTACT_ADDRESS.CONTACT_ADDRESS);
-    const events = await contratoInteligente.getPastEvents();
-    console.log(events)
-    res.status(200).send(events);
-  } catch (e) {
-    console.error(e)
-  } 
+    contratoInteligente.getPastEvents(
+      "TransferSingle",
+          { fromBlock: 0, toBlock: 'latest' },
+          (err, events) => {
+          res.status(200).send(JSON.stringify(events));
+          }
+      );
+    } catch (e) {
+      console.error(e)
+    }   
 });
 
 app.get('/saldo', async function(req, res) {
   let conta = req.query.conta;  
   console.log("conta: ", conta)
 
-  var web3 = new Web3('http://127.0.0.1:8545');
+  var web3 = new Web3(address);
   var contratoInteligente = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI, CONTACT_ADDRESS.CONTACT_ADDRESS);
 
-  let owner = await contratoInteligente.methods.balanceOf(conta,"1").call(function (err, res) {
+  let saldo = await contratoInteligente.methods.balanceOf(conta,"1").call(function (err, res) {
     if (err) {
       console.log("Ocorreu um erro", err)
       return
     }
-    console.log("O saldo é: ", res)
+    console.log("Saldo gerado com Sucesso")
   }); 
-  res.status(200).send(owner);
-
+  res.status(200).send(JSON.stringify(saldo));
 });
 
 app.post('/emitir', async function(req, res) {
-
   let account = req.body.account;
   let id = req.body.id;
   let amount = req.body.amount;
   let data = req.body.data;
-  
-
   const network = process.env.ETHEREUM_NETWORK;
-
   const web3 = new Web3(
     new Web3.providers.HttpProvider(
-      `http://127.0.0.1:8545`
+      `${address}`
     )
   );
   const signer = web3.eth.accounts.privateKeyToAccount(
@@ -83,7 +81,6 @@ app.post('/emitir', async function(req, res) {
   var contratoInteligente = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI, CONTACT_ADDRESS.CONTACT_ADDRESS);
   
   const tx = contratoInteligente.methods.mint(account,id,amount,data);
-
   const receipt = await tx
     .send({
       from: signer.address,
@@ -106,7 +103,7 @@ app.post('/queimar', async function(req, res) {
 
   const web3 = new Web3(
     new Web3.providers.HttpProvider(
-      `http://127.0.0.1:8545`
+      `${address}`
     )
   );
   const signer = web3.eth.accounts.privateKeyToAccount(
@@ -142,7 +139,7 @@ app.post('/transferir', async function(req, res) {
 
   const web3 = new Web3(
     new Web3.providers.HttpProvider(
-      `http://127.0.0.1:8545`
+      `${address}`
     )
   );
   const signer = web3.eth.accounts.privateKeyToAccount(
@@ -163,6 +160,46 @@ app.post('/transferir', async function(req, res) {
     });
     console.log(`Moeda queimada no bloco ${receipt.blockNumber}`);
   res.status(200).send(`Moeda transferida com sucesso no bloco ${receipt.blockNumber}`);
+});
+
+app.post('/conta', async function(req, res) {
+  try {
+    const network = process.env.ETHEREUM_NETWORK;
+
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider(
+        `${address}`
+      )
+    );
+
+    let senha = req.body.from;
+    let account = web3.eth.accounts.create();
+    console.log(`Resultado da criacao da conta: ${account.address} ${account.privateKey}`);
+    let wallet = web3.eth.accounts.wallet.create();
+    // console.log(`Resultado da criacao da wallet: ${wallet.defaultKeyName}`);
+    let walletAccount = web3.eth.accounts.wallet.add(account);
+    // Armazena a carteira criptografada no armazenamento local. 
+    // web3.eth.accounts.wallet.save('senha');
+    // console.log('Carteira salva');
+    console.log(`Wallet: ${walletAccount.address}`);
+    res.status(200).send(`Wallet: ${walletAccount.address} \nChave publica: ${account.privateKey}`);
+  } catch (e) {
+    console.error(e)
+  }   
+});
+
+app.post('/carimbo', async function(req, res) {
+  try {
+    let bloco = req.body.block; 
+    var web3 = new Web3(address);
+    var contratoInteligente = new web3.eth.Contract(CONTACT_ABI.CONTACT_ABI, CONTACT_ADDRESS.CONTACT_ADDRESS);
+    let block = await web3.eth.getBlock(bloco);
+    let timestamp = block.timestamp;
+    var date = new Date(timestamp * 1000);
+    res.status(200).send(`${date}`);        
+    } catch (e) {
+          console.error(e)
+    }   
 });
 
 app.listen(3001, () => {
